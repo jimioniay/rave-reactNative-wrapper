c4ed8754371795c7079dd47c33e9b69fbf648d28
Ayomide Jimi-Oni <ayomidejimi-oni@Ayomides-MacBook-Pro.local>
2020-03-18 01:49:17 +0300

    change webview version

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
-import {Modal, 
-        Text, 
-        Alert,
-        View, 
-        TouchableOpacity, 
-        ActivityIndicator,
-        Platform,} from 'react-native';
+import React, { useState } from 'react';
+import {
+  Modal,
+  Text,
+  Alert,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+  Platform,
+} from 'react-native';
 
 import { WebView } from 'react-native-webview';
 import decode from 'urldecode';
@@ -16,14 +18,13 @@ let isLoading;
 let setIsLoading;
 
 export const Rave = props => {
+  console.log(props);
   [showModal, setShowModal] = useState(false);
   [isLoading, setIsLoading] = useState(false);
-//  this._onNavigationStateChange = this._onNavigationStateChange.bind(this)
-
-
+  //  this._onNavigationStateChange = this._onNavigationStateChange.bind(this)
 
   let Rave = {
-      html:  `
+    html: `
         <!DOCTYPE html>
         <html lang="en">
           <head>
@@ -41,18 +42,24 @@ export const Rave = props => {
                 var x = getpaidSetup({
                   PBFPubKey: API_publicKey,
                   amount: ${props.amount},
-                  customer_phone: "${(props.customerPhone || '')}",
-                  customer_email: "${(props.customerEmail || '')}",
+                  customer_phone: "${props.customerPhone || ''}",
+                  customer_email: "${props.customerEmail || ''}",
                   custom_description: "${props.contentDescription}",
-                  currency: "${(props.currency || "NGN")}",
-                  custom_logo:"${(props.custom_logo || "")}",
+                  currency: "${props.currency || 'NGN'}",
+                  custom_logo:"${props.custom_logo || ''}",
                   txref: "${props.txref}",
                   meta: [{
-                      metaname: "${props.billingName || props.customerEmail || ''}",
-                      metavalue: "${props.billingMobile || props.customerPhone || ''}"
+                      metaname: "${props.billingName ||
+                        props.customerEmail ||
+                        ''}",
+                      metavalue: "${props.billingMobile ||
+                        props.customerPhone ||
+                        ''}"
                   }],
                   onclose: function(data) {
-                    var resp = {event: "cancelled", data: {txRef: ${props.txref}, flwRef: dataç, status:""}};
+                    var resp = {event: "cancelled", data: {txRef: ${
+                      props.txref
+                    }, flwRef: dataç, status:""}};
                     window.ReactNativeWebView.postMessage(JSON.stringify(resp))
                   },
                  
@@ -62,9 +69,13 @@ export const Rave = props => {
                           response.tx.chargeResponseCode == "00" ||
                           response.tx.chargeResponseCode == "0"
                       ) {
-                           var resp = {event: "successful", data: {txRef: ${props.txref}, ...response}};
+                           var resp = {event: "successful", data: {txRef: ${
+                             props.txref
+                           }, ...response}};
                       } else {
-                        var resp = {event: "successful", data: {txRef: ${props.txref}, ...response}};
+                        var resp = {event: "successful", data: {txRef: ${
+                          props.txref
+                        }, ...response}};
                         window.ReactNativeWebView.postMessage(JSON.stringify(resp))
                       }
                       x.close();
@@ -74,179 +85,227 @@ export const Rave = props => {
             </script>
           </body>
         </html>
-      `
-    }
+      `,
+  };
 
-    if(Platform.OS === 'ios'){
-      return (
-        <View>
-          <Modal
-            visible={showModal}
-            style={{backgroundColor: 'red'}}
-            animationType="slide"
-            transparent={false}>
-            
-                <WebView
-                javaScriptEnabled={true}
-                javaScriptEnabledAndroid={true}
-                useWebKit={true}
-                originWhitelist={['*']}
-                ref={( webView ) => this.MyWebView = webView}
-                source={Rave}
-                onMessage={(e)=>{messageRecived(props, e.nativeEvent.data)}}
-                onLoadStart={() => setIsLoading(true)}
-                onLoadEnd={() => setIsLoading(false)}
-                onNavigationStateChange={async data => {
-                  if (data.url.includes('https://api.ravepay.co') || data.url.includes('https://ravesandboxapi.flutterwave.com')){
-                    await messageRecived(props,data);
-            }      
-                }}
-              />
-             
-              {/*Start of Loading modal*/}
-              {
-                isLoading === true && (
-                  <View style={{position: 'absolute', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center', top: 0}}>
-                    <ActivityIndicator size="large" color={props.ActivityIndicatorColor || '#f5a623'} />
-                    <TouchableOpacity
-                      style={{
-                        position: 'absolute',
-                        top: 20,
-                        right: 20,
-                        backgroundColor: '#f5a623',
-                        borderRadius: 4,
-                        padding: 10,
-                        paddingTop: 7,
-                        paddingBottom: 7
-                      }}
-                      onPress={() => {
-                        setShowModal(false)
-                        setIsLoading(false)
-                        props.onCancel();
-                      }}>
-                      <Text style={{color: 'white', fontSize: 12, fontWeight: 'bold'}}>Cancel</Text>
-                    </TouchableOpacity>
-                  </View>
-                )
+  if (Platform.OS === 'ios') {
+    return (
+      <View>
+        <Modal
+          visible={showModal}
+          style={{ backgroundColor: 'red' }}
+          animationType="slide"
+          transparent={false}
+        >
+          <WebView
+            javaScriptEnabled={true}
+            javaScriptEnabledAndroid={true}
+            useWebKit={true}
+            originWhitelist={['*']}
+            ref={webView => (this.MyWebView = webView)}
+            source={Rave}
+            onMessage={e => {
+              messageRecived(props, e.nativeEvent.data);
+            }}
+            onLoadStart={() => setIsLoading(true)}
+            onLoadEnd={() => setIsLoading(false)}
+            onNavigationStateChange={async data => {
+              if (
+                data.url.includes('https://api.ravepay.co') ||
+                data.url.includes('https://ravesandboxapi.flutterwave.com')
+              ) {
+                await messageRecived(props, data);
               }
-          </Modal>
-          <TouchableOpacity style={props.btnStyles} onPress={() => setShowModal(true)}>
-            <Text style={props.textStyles}>{props.buttonText}</Text>
-          </TouchableOpacity>
-        </View>
-      );
-    } else{
-      return (
-        <View>
-          <Modal
-            visible={showModal}
-            style={{backgroundColor: 'red'}}
-            animationType="slide"
-            transparent={false}>
-              
-                <WebView
-                javaScriptEnabled={true}
-                javaScriptEnabledAndroid={true}
-                originWhitelist={['*']}
-                ref={( webView ) => this.MyWebView = webView}
-                source={Rave}
-                onMessage={(e)=>{
-                  messageRecived({onCancel: props.onCancel, onSuccess: props.onSuccess, onError: props.onError}, e.nativeEvent.data)
-                }}
-                onLoadStart={() => setIsLoading(true)}
-                onLoadEnd={() => setIsLoading(false)}
-                onNavigationStateChange={ async data => {
-                  if (data.url.includes('https://api.ravepay.co') || data.url.includes('https://ravesandboxapi.flutterwave.com')){
-                    await messageRecived(props,data);
-            }
-                }}
+            }}
+          />
+
+          {/*Start of Loading modal*/}
+          {isLoading === true && (
+            <View
+              style={{
+                position: 'absolute',
+                height: '100%',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+                top: 0,
+              }}
+            >
+              <ActivityIndicator
+                size="large"
+                color={props.ActivityIndicatorColor || '#f5a623'}
               />
-             
-              {/*Start of Loading modal*/}
-              {
-                isLoading === true && (
-                  <View style={{position: 'absolute', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center', top: 0}}>
-                    <ActivityIndicator size="large" color={props.ActivityIndicatorColor || '#f5a623'} />
-                    <TouchableOpacity
-                      style={{
-                        position: 'absolute',
-                        top: 20,
-                        right: 20,
-                        backgroundColor: '#f5a623',
-                        borderRadius: 4,
-                        padding: 10,
-                        paddingTop: 7,
-                        paddingBottom: 7
-                      }}
-                      onPress={() => {
-                        setShowModal(false)
-                        setIsLoading(false)
-                        props.onCancel();
-                      }}>
-                      <Text style={{color: 'white', fontSize: 12, fontWeight: 'bold'}}>Cancel</Text>
-                    </TouchableOpacity>
-                  </View>
-                )
+              <TouchableOpacity
+                style={{
+                  position: 'absolute',
+                  top: 20,
+                  right: 20,
+                  backgroundColor: '#f5a623',
+                  borderRadius: 4,
+                  padding: 10,
+                  paddingTop: 7,
+                  paddingBottom: 7,
+                }}
+                onPress={() => {
+                  setShowModal(false);
+                  setIsLoading(false);
+                  props.onCancel();
+                }}
+              >
+                <Text
+                  style={{ color: 'white', fontSize: 12, fontWeight: 'bold' }}
+                >
+                  Cancel
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Modal>
+        <TouchableOpacity
+          style={props.btnStyles}
+          onPress={() => setShowModal(true)}
+        >
+          <Text style={props.textStyles}>{props.buttonText}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Modal
+          visible={showModal}
+          style={{ backgroundColor: 'red' }}
+          animationType="slide"
+          transparent={false}
+        >
+          <WebView
+            javaScriptEnabled={true}
+            javaScriptEnabledAndroid={true}
+            originWhitelist={['*']}
+            ref={webView => (this.MyWebView = webView)}
+            source={Rave}
+            onMessage={e => {
+              messageRecived(
+                {
+                  onCancel: props.onCancel,
+                  onSuccess: props.onSuccess,
+                  onError: props.onError,
+                },
+                e.nativeEvent.data,
+              );
+            }}
+            onLoadStart={() => setIsLoading(true)}
+            onLoadEnd={() => setIsLoading(false)}
+            onNavigationStateChange={async data => {
+              if (
+                data.url.includes('https://api.ravepay.co') ||
+                data.url.includes('https://ravesandboxapi.flutterwave.com')
+              ) {
+                await messageRecived(props, data);
               }
-          </Modal>
-          <TouchableOpacity style={props.btnStyles} onPress={() => setShowModal(true)}>
-            <Text style={props.textStyles}>{props.buttonText}</Text>
-          </TouchableOpacity>
-        </View>
-      );
-    }
+            }}
+          />
 
-    
-}
+          {/*Start of Loading modal*/}
+          {isLoading === true && (
+            <View
+              style={{
+                position: 'absolute',
+                height: '100%',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+                top: 0,
+              }}
+            >
+              <ActivityIndicator
+                size="large"
+                color={props.ActivityIndicatorColor || '#f5a623'}
+              />
+              <TouchableOpacity
+                style={{
+                  position: 'absolute',
+                  top: 20,
+                  right: 20,
+                  backgroundColor: '#f5a623',
+                  borderRadius: 4,
+                  padding: 10,
+                  paddingTop: 7,
+                  paddingBottom: 7,
+                }}
+                onPress={() => {
+                  setShowModal(false);
+                  setIsLoading(false);
+                  props.onCancel();
+                }}
+              >
+                <Text
+                  style={{ color: 'white', fontSize: 12, fontWeight: 'bold' }}
+                >
+                  Cancel
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Modal>
+        <TouchableOpacity
+          style={props.btnStyles}
+          onPress={() => setShowModal(true)}
+        >
+          <Text style={props.textStyles}>{props.buttonText}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+};
 
-const parseResponse = (props , data )=> {
+const parseResponse = (props, data) => {
   let response = {
     txRef: props.txref,
     flwRef: '',
     status: '',
   };
-  if (data.url === undefined) { // handle sandbox
+  if (data.url === undefined) {
+    // handle sandbox
     let url = new URL(data);
     response = {
       txRef: props.txref,
       flwRef: url.searchParams.get('ref'),
       status: url.searchParams.get('message'),
     };
-  }
-  else if (data.url.includes('https://ravesandboxapi.flutterwave.com')) {
+  } else if (data.url.includes('https://ravesandboxapi.flutterwave.com')) {
     let regex = /[?&]([^=#]+)=([^&#]*)/g,
-        params = {},
-        match=[];   
-    let values=[];
-    for (let i = 0; i < 4; i ++){
+      params = {},
+      match = [];
+    let values = [];
+    for (let i = 0; i < 4; i++) {
       match = regex.exec(data.url);
       values.push(match[2]);
     }
     response = {
       txRef: props.txref,
-      flwRef:values[0],
+      flwRef: values[0],
       status: values[2],
-    } 
-  }
-  else { // handle live
+    };
+  } else {
+    // handle live
     let regex = /[?&]([^=#]+)=([^&#]*)/g,
-        params = {},
-        match;
+      params = {},
+      match;
     match = regex.exec(decode(data.url));
     let parsedResp = JSON.parse(match[2]);
     response = {
       txRef: props.txref,
       flwRef: parsedResp.transactionreference,
-      status: parsedResp.responsemessage
-    }
+      status: parsedResp.responsemessage,
+    };
   }
   return response;
 };
 
-
 const messageRecived = async (props, data) => {
-  let parsedData = typeof data === 'object' ? "" :  JSON.parse(data);
-  switch(parsedData.event){
+  let parsedData = typeof data === 'object' ? '' : JSON.parse(data);
+  switch (parsedData.event) {
     case 'cancelled':
       await setShowModal(false);
       return props.onCancel(parsedData.data);
@@ -255,11 +314,11 @@ const messageRecived = async (props, data) => {
       return props.onSuccess(parsedData.data);
     default:
       try {
-        await setShowModal(false);  
-        await props.onSuccess(parseResponse(props,data));
+        await setShowModal(false);
+        await props.onSuccess(parseResponse(props, data));
       } catch (error) {
         console.log(error);
-        await props.onError(`${data} || ${error}` );
+        await props.onError(`${data} || ${error}`);
       }
   }
-}
+};
